ce28da9ecf4149153ff7a70a323838b663b7ea52
nicosaadjian <nicosaadjian@gmail.com>
2025-08-23 21:42:02 -0300

    updating files

--- scripts/common_utils.py
+++ scripts/common_utils.py
@@ -26,3 +26,18 @@ def transform_metro_data(df, rename_dict):
 
 # loader
 # definir el loader con snowflake connector o similar
+
+def load_data_to_aws_rds(df, conn):
+    # insert into tempdb.dbo.test_table(df) ---> write_pandas.tools
+    # Implementar la lógica para cargar el DataFrame en Snowflake usando el conector proporcionado
+    pass
+
+def extract_transform_load(csv_file, rename_dict, conn):
+    print("Beginning ETL process: extracting data from CSV.\n")
+    df = extract_csv_data(csv_file)
+    print("ETL process: transforming data from CSV.\n")
+    df = transform_metro_data(df, rename_dict)
+    print("Ending ETL process: inserting transformed data from CSV into AWS RDS.\n")
+    load_data_to_aws_rds(df, conn)
+    
+    return df
--- scripts/run_metro_pipeline.py
+++ scripts/run_metro_pipeline.py
@@ -1,42 +1,45 @@
-import pandas as pd
-from scripts.common_utils import extract_csv_data, transform_metro_data
+from scripts.common_utils import extract_transform_load
+from utils.conexion import connect_to_sql_server
+from utils.credentials import server, database, username, password
 
 # el error del interpreter lo pude resolver agregando el path del interpreter a settings.json y borrando la parte de data_pipeline al inicio del import
 
 def run_metro_pipeline():
 
-    subte_csv = r'C:\Users\nicos\OneDrive\Escritorio\data_pipeline\data\bocas-de-subte.csv'
+    connect_to_sql_server(server, database, username, password)
 
+    subte_csv = r'C:\\Users\\nicos\\Desktop\\pruebas_aws\\data_pipeline_aws\data\bocas-de-subte.csv'
     rename_dict = {
-    'long': 'Longitud', 
-    'lat' : 'Latitud', 
-    'id'  : 'Id', 
-    'linea': 'Linea', 
-    'estacion': 'Estacion', 
-    'numero_de_': 'Numero_De', 
-    'destino_bo' : 'Destino_Bo',
-    'lineas_de_' : 'Lineas_De', 
-    'cierra_fin' : 'Cierra_Fin', 
-    'escalera_p' : 'Escalera_P', 
-    'escalera_m' : 'Escalera_M', 
-    'ascensor' : 'Ascensor',
-    'rampa' : 'Rampa', 
-    'salvaescal' : 'SalvaEscalera', 
-    'calle' : 'Calle', 
-    'altura' : 'Altura', 
-    'calle2' : 'Calle2', 
-    'barrio' : 'Barrio', 
-    'comuna' : 'Columna',
-    'observacio' : 'Observacion', 
-    'Objeto' : 'Objeto', 
-    'dom_norma' : 'Dom_Norma', 
-    'dom_orig' : 'Dom_Origin'
-}
+            'long': 'Longitud', 
+            'lat' : 'Latitud', 
+            'id'  : 'Id', 
+            'linea': 'Linea', 
+            'estacion': 'Estacion', 
+            'numero_de_': 'Numero_De', 
+            'destino_bo' : 'Destino_Bo',
+            'lineas_de_' : 'Lineas_De', 
+            'cierra_fin' : 'Cierra_Fin', 
+            'escalera_p' : 'Escalera_P', 
+            'escalera_m' : 'Escalera_M', 
+            'ascensor' : 'Ascensor',
+            'rampa' : 'Rampa', 
+            'salvaescal' : 'SalvaEscalera', 
+            'calle' : 'Calle', 
+            'altura' : 'Altura', 
+            'calle2' : 'Calle2', 
+            'barrio' : 'Barrio', 
+            'comuna' : 'Comuna',
+            'observacio' : 'Observacion', 
+            'Objeto' : 'Objeto', 
+            'dom_norma' : 'Dom_Norma', 
+            'dom_orig' : 'Dom_Origin'
+        }
 
-    df = extract_csv_data(subte_csv)
-    df = transform_metro_data(df, rename_dict)
     
-    return df
+    extract_transform_load(subte_csv, rename_dict)
+
+    
+
 
 if __name__ == 'main':
-    df = run_metro_pipeline()
+    run_metro_pipeline()
